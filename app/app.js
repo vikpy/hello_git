b2a598d384c1a7f65dca0a55378d664f780f8bdf
vikraantpai <vikraant.py@gmail.com>
2018-10-16 23:46:27 +0530

    added rest of the code to app.js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,46 @@
-const restify = require('restify') ;
-const adapter =  require('botbuilder');
+
+const restify = require("restify");
+const botbuilder = require("botbuilder");
+const fs = require('fs');  
+
+
+// const options = {  
+//     key: fs.readFileSync('./server_key.pem', 'utf8'),  
+//     certificate: fs.readFileSync('./server.pem', 'utf8')  
+// }; 
+
+//options
+//Setting up restify server 
+const server = restify.createServer(); 
+server.listen(
+//port 
+3978, 
+
+//callback
+() => {
+	console.log(`${server.name} listening to ${server.url}`);
+}
+);
+
+//Create chat connector for communication with the bot framework 
+const adapter = new botbuilder.BotFrameworkAdapter({
+	appId : process.env.SKYPE_BOT_ID_SVC, 
+	appPassword : process.env.SKYPE_BOT_PASSWORD_SVC
+});
 
 
 
 
+server.post('/foodiebot', (req, res) => {
+    // Use the adapter to process the incoming web request into a TurnContext object.
+    adapter.processActivity(req, res, async (turnContext) => {
+        // Do something with this incoming activity!
+        if (turnContext.activity.type === 'message') {            
+            // Get the user's text
+            const utterance = turnContext.activity.text;
+ 
+            // send a reply
+            await turnContext.sendActivity(`I heard you say ${ utterance }`);
+        }
+    });
+});
